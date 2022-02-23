e1d1e1aa8fb1d4ce935912a083d8734bb4191fef
Shital Rahane <shitalrahane1234@gmail.com>
2022-02-23 16:57:51 +0530

    updated text

--- App.js
+++ App.js
@@ -72,7 +72,7 @@ const App: () => Node = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>Appkkkkkkk`ß`.js</Text> to change this
-            screen and then come back to sevvvhdfkshf.
+            screen and then come back to sevvvhdfkshf. ij yodatubg
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
