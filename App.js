2e952779fbcef99674d07b25405f2515b5e596e0
Shital Rahane <shitalrahane1234@gmail.com>
2022-02-23 17:58:18 +0530

    changes to the app.js

--- App.js
+++ App.js
@@ -73,7 +73,7 @@ const App: () => Node = () => {
           <Section title="Step One">
             Edit <Text style={styles.highlight}>Appkkkkkkk`ß`.js</Text> to change this
             screen and then come back to sevvvhdfkshf. ij yodatubg
-            After changing password
+            After changing password dddd
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
